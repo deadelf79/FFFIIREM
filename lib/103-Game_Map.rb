122c059111eb2de412b6fdc8999062d41c766f7f
Эльф <deadelf79@gmail.com>
2014-12-10 17:20:39 +0300

    Game map :globe_with_meridians:

---
+++ lib/103-Game_Map.rb
@@ -0,0 +1,22 @@
+﻿class Game_Map
+	def initialize(filename)
+		@data=[]
+		if FileTest.exist?('dat/'+filename)
+			_load_datas
+		else
+			_create_datas
+		end
+	end
+	
+	private
+	
+	def _load_datas
+	
+	end
+	
+	def _create_datas
+		20.times{ |x|
+			@data<<Array.new(15,0)
+		}
+	end
+end
